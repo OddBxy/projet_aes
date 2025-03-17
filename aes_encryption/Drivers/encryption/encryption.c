011767cf206de6473dd85dd650f457250a70a4c6
OddBxy <theo.lanier@isen.yncrea.fr>
2025-03-17 14:50:04 +0100

    fix addroundkey

--- aes_encryption/Drivers/encryption/encryption.c
+++ aes_encryption/Drivers/encryption/encryption.c
@@ -60,13 +60,11 @@ uint8_t gmult(uint8_t a, uint8_t b) {
 void add_roundKey(uint8_t *buffer, uint8_t *roundKey){
 
 	for(int i=0; i<4; i++){
-		buffer[4*0 + i] = buffer[i]^roundKey[i];				//colonne i ligne 0
-		buffer[4*1 + i] = buffer[1 + i]^roundKey[1 + i];		//colonne i ligne 1
-		buffer[4*2 + i] = buffer[2 + i]^roundKey[2 + i];		//colonne i ligne 2
-		buffer[4*3 + i] = buffer[3 + i]^roundKey[3 + i];		//colonne i ligne 3
+		buffer[4*i + 0] = buffer[4*i + 0]^roundKey[4*i + 0];
+		buffer[4*i + 1] = buffer[4*i + 1]^roundKey[4*i + 1];
+		buffer[4*i + 2] = buffer[4*i + 2]^roundKey[4*i + 2];
+		buffer[4*i + 3] = buffer[4*i + 3]^roundKey[4*i + 3];
 	}
-
-	//buffers sont 1d mais on les traites 2d en colonnes de 4
 }
 
 
