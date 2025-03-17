1d7e16fd47e71bbba297b44f111c4f221ecbcb95
OddBxy <theo.lanier@isen.yncrea.fr>
2025-03-17 08:38:12 +0100

    finish init

---
+++ aes_encryption/Drivers/decryption/decryption.h
@@ -0,0 +1,13 @@
+/*
+ * decryption.h
+ *
+ *  Created on: Mar 17, 2025
+ *      Author: theo0
+ */
+
+#ifndef DECRYPTION_DECRYPTION_H_
+#define DECRYPTION_DECRYPTION_H_
+
+
+
+#endif /* DECRYPTION_DECRYPTION_H_ */
---
+++ aes_encryption/Drivers/encryption/encryption.h
@@ -0,0 +1,13 @@
+/*
+ * encryption.h
+ *
+ *  Created on: Mar 17, 2025
+ *      Author: theo0
+ */
+
+#ifndef ENCRYPTION_ENCRYPTION_H_
+#define ENCRYPTION_ENCRYPTION_H_
+
+
+
+#endif /* ENCRYPTION_ENCRYPTION_H_ */
